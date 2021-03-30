9a408072a2aeb24dc7706754f11e25a38030bda8
ezherman <elh605@york.ac.uk>
2021-03-30 14:01:27 +0100

    select and describe variables, issue /issues/3

--- bin/obtain_data.R
+++ bin/obtain_data.R
@@ -1,22 +1,39 @@
 library(NHANES)
 library(dplyr)
 
-#proportions representing a simple random sample
+# proportions representing a simple random sample
 prop <- as.numeric(table(NHANES$Race1)/nrow(NHANES))
 
-set.seed(1000) #reproducible
+set.seed(1000) # reproducible
 
-#take sample from NHANESraw that represents a simple random sample
+# take sample from NHANESraw that represents a simple random sample
 dat <- NHANESraw %>%
   
-  #add sample weights
+  # add sample weights
   mutate(weight = case_when(Race1 == "Black" ~ prop[1],
                             Race1 == "Hispanic" ~ prop[2],
                             Race1 == "Mexican" ~ prop[3],
                             Race1 == "White" ~ prop[4],
                             Race1 == "Other" ~ prop[5])) %>%
   group_by(Race1) %>%
-  sample_n(10000 * weight) %>% #sample from each according to prop to obtain 10000 obvs in total
-  select(-weight) #remove weighting column
+  sample_n(10000 * weight) %>% # sample from each according to prop to obtain 10000 obvs in total
+  select(-c(weight, 
+            WTINT2YR, WTMEC2YR, 
+            SDMVPSU, SDMVSTRA)) %>% # remove weighting columns
+  select(-c(SurveyYr, HHIncomeMid,
+            Length, HeadCirc,
+            BMICatUnder20yrs,
+            BPSys1, BPSys2,
+            BPDia1, BPDia2,
+            BPSys3, BPDia3,
+            UrineVol2,
+            UrineFlow2,
+            PregnantNow)) %>% # remove variables which will not be used
+  select(-c(AgeMonths, Race3, 
+            Testosterone,
+            TVHrsDay, 
+            CompHrsDay,
+            TVHrsDayChild,
+            CompHrsDayChild)) # remove data which was only recorded for one out of two survey rounds
 
 rm(prop)
